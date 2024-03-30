879721f6136d0cf982bbfe8257193c88109e9910
gamepat <27729666+gamepat@users.noreply.github.com>
2024-03-30 19:17:43 +0100

    Added try-catch cause why not

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -7,17 +7,23 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     hello();
 
-    auto client = modbus::create("127.0.0.1:5502");
+    try {
+        auto client = modbus::create("127.0.0.1:5502");
 
-    auto input_reg = client->read_input_registers(0, 2);
+        auto input_reg = client->read_input_registers(0, 2);
 
-    auto holding_reg = client->read_holding_registers(0, 4);
+        auto holding_reg = client->read_holding_registers(0, 4);
 
-    std::vector<uint16_t> x{2, 3};
-    rust::Slice<const uint16_t> s(x.data(), x.size());
-    client->write_registers(1, s);
+        std::vector<uint16_t> x{2, 3};
+        rust::Slice<const uint16_t> s(x.data(), x.size());
+        client->write_registers(1, s);
 
-    auto holding_reg2 = client->read_holding_registers(0, 4);
+        auto holding_reg2 = client->read_holding_registers(0, 4);
+    }
+    catch (const std::exception &ex) {
+        std::cout << "Caught exception: " << ex.what() << std::endl;
+        return -1;
+    }
 
     return 0;
 }
